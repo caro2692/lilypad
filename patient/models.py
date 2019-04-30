0cac3b9578bdca966375ced0f06bdc07da415a39
Todd Bracken <todd@sighten.io>
2019-04-29 19:43:25 -0600

    :construction:add initial patient fields and migrations

--- patient/models.py
+++ patient/models.py
@@ -2,4 +2,7 @@ from django.db import models
 
 
 class Patient(models.Model):
-    patient_id = models.CharField(max_length=200)
+    name = models.CharField(max_length=128)
+    active = models.BooleanField(default=True)
+    tidepool_user_id = models.CharField(max_length=36, null=True, blank=True)
+    tidepool_username = models.CharField(max_length=56, null=True, blank=True)
