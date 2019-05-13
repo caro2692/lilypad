8e5a89f728b701edcd1cd933e99fb927f8faf5a1
Todd Bracken <todd@sighten.io>
2019-05-13 16:21:16 -0600

    :checkered_flag:use axios to display patient list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios'
 import logo from './logo.png'
 import { Container, Menu, Table } from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header>
-        <Menu fixed="top" size="large" inverted borderless className="App-header">
-          <Container>
-            <Menu.Item>
-              <img src={logo} alt="logo" />
-            </Menu.Item>
-            <Menu.Item>Lilypad</Menu.Item>
-          </Container>
-        </Menu>
-      </header>
-      <Container className="main">
-        <Table celled>
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      patients: []
+    }
+  }
 
-          <Table.Header>
-            <Table.Row>
-              <Table.HeaderCell>Header</Table.HeaderCell>
-              <Table.HeaderCell>Header</Table.HeaderCell>
-              <Table.HeaderCell>Header</Table.HeaderCell>
-            </Table.Row>
-          </Table.Header>
+  componentDidMount() {
+    this.getPatients()
+  }
 
-          <Table.Body>
-            <Table.Row>
-              <Table.Cell>blah</Table.Cell>
-              <Table.Cell>blah</Table.Cell>
-              <Table.Cell>blah</Table.Cell>
-            </Table.Row>
-          </Table.Body>
+  getPatients = () => {
+    axios.get('/api/patient/')
+      .then(resp => this.setState({ patients: resp.data }))
+      .catch(err => console.log(err))
+  }
 
-        </Table>
-      </Container>
-    </div>
-  );
+  renderPatientList = () => {
+    const patients = this.state.patients.filter(p => p.active)
+    return patients.map(patient => (
+      <Table.Row key={patient.id}>
+        <Table.Cell>{ patient.name }</Table.Cell>
+        <Table.Cell>{ patient.tidepool_username }</Table.Cell>
+        <Table.Cell>{ patient.tidepool_userid }</Table.Cell>
+      </Table.Row>
+    ))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <Menu fixed="top" size="large" inverted borderless className="App-header">
+            <Container>
+              <Menu.Item>
+                <img src={logo} alt="logo" />
+              </Menu.Item>
+              <Menu.Item>Lilypad</Menu.Item>
+            </Container>
+          </Menu>
+        </header>
+        <Container className="main">
+          <h1>Patients</h1>
+
+          <Table celled>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Name</Table.HeaderCell>
+                <Table.HeaderCell>Tidepool Username</Table.HeaderCell>
+                <Table.HeaderCell>Tidepool ID</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+
+            <Table.Body>
+              {this.renderPatientList()}
+            </Table.Body>
+
+          </Table>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
